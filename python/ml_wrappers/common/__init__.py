62be433a57e00f0f8619aa5017edbf6e953650d1
Ilya Matiach <ilmat@microsoft.com>
2021-12-13 14:33:49 -0500

    initial commit

---
+++ python/ml_wrappers/common/__init__.py
@@ -0,0 +1,5 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Defines a common directory shared across ML model and dataset wrappers."""
