a78d273f83a4553eba712ba72fd751aa3c96ef7b
Ilya Matiach <ilmat@microsoft.com>
2022-01-13 10:46:53 -0500

    fix imports in model init file to include WrappedClassificationModel and (#32)
    WrappedRegressionModel

--- python/ml_wrappers/model/__init__.py
+++ python/ml_wrappers/model/__init__.py
@@ -4,6 +4,8 @@
 
 """Common infrastructure, class hierarchy and utilities for model explanations."""
 
-from .model_wrapper import WrappedPytorchModel, _wrap_model, wrap_model
+from .model_wrapper import (WrappedClassificationModel, WrappedPytorchModel,
+                            WrappedRegressionModel, _wrap_model, wrap_model)
 
-__all__ = ['_wrap_model', 'wrap_model', 'WrappedPytorchModel']
+__all__ = ['WrappedClassificationModel', 'WrappedPytorchModel',
+           'WrappedRegressionModel', '_wrap_model', 'wrap_model']
