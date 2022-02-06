6d4f12eb4dd9f24e6654242b2d6fe27fb63ec104
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2022-02-06 13:56:38 +0530

    Updated Syntax View in `setup.py` file

--- python/setup.py
+++ python/setup.py
@@ -12,10 +12,9 @@ with open('ml_wrappers/version.py') as f:
     code = compile(f.read(), f.name, 'exec')
     exec(code)
 
-README_FILE = 'README.md'
-LICENSE_FILE = 'LICENSE.txt'
+README_FILE = "README.md"
+LICENSE_FILE = "LICENSE.txt"
 
-# Note: used when generating the wheel but not on pip install of the package
 if os.path.exists('../LICENSE'):
     shutil.copyfile('../LICENSE', LICENSE_FILE)
 
@@ -47,17 +46,17 @@ with open(README_FILE, 'r', encoding='utf-8') as f:
     README = f.read()
 
 setup(
-    name=name,  # noqa: F821
-    version=version,  # noqa: F821
-    description='Machine Learning Wrappers SDK for Python',
-    long_description=README,
-    long_description_content_type='text/markdown',
-    author='Microsoft Corp',
-    author_email='ilmat@microsoft.com',
-    license='MIT License',
-    url='https://github.com/microsoft/ml-wrappers',
-    classifiers=CLASSIFIERS,
-    packages=find_packages(exclude=["*.tests"]),
-    install_requires=DEPENDENCIES,
-    zip_safe=False
+    name = name,  # noqa: F821
+    version = version,  # noqa: F821
+    description = "Machine Learning Wrappers SDK for Python",
+    long_description = README,
+    long_description_content_type = "text/markdown",
+    author = 'Microsoft Corp',
+    author_email = 'ilmat@microsoft.com',
+    license = 'MIT License',
+    url = 'https://github.com/microsoft/ml-wrappers',
+    classifiers = CLASSIFIERS,
+    packages = find_packages(exclude=["*.tests"]),
+    install_requires = DEPENDENCIES,
+    zip_safe = False
 )
