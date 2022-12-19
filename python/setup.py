a3d9dcb6b2aa49f65e18939cfa332ada830bce82
Gaurav Gupta <gaugup@microsoft.com>
2022-12-19 10:27:25 -0800

    Add python 3.10 to setup.py and docs
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>

--- python/setup.py
+++ python/setup.py
@@ -30,6 +30,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Topic :: Scientific/Engineering :: Artificial Intelligence',
     'Operating System :: Microsoft :: Windows',
     'Operating System :: MacOS',
