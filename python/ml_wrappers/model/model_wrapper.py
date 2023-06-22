2731c2d1788f1d93475e69ce397643add9520f4f
Natalie Isak <natalieisak@microsoft.com>
2023-06-22 17:13:30 -0400

    removing excess

--- python/ml_wrappers/model/model_wrapper.py
+++ python/ml_wrappers/model/model_wrapper.py
@@ -38,17 +38,9 @@ module_logger.setLevel(logging.INFO)
 
 try:
     import torch.nn as nn
-    import torch
 except ImportError:
     module_logger.debug(
         'Could not import torch, required if using a PyTorch model')
-
-
-def auto_device():
-    """
-    Determines available device. If GPU is available, then cuda. Else, cpu.
-    """
-    return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 def wrap_model(model, examples, model_task: str = ModelTask.UNKNOWN,
