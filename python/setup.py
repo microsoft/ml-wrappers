48c7f37d72f93674c134848e6c66cd4d42a2d46b
Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
2023-06-30 14:17:34 -0700

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -42,7 +42,7 @@ DEPENDENCIES = [
     'pandas<2.0.0',
     'scipy',
     'scikit-learn',
-    joblib<1.3.0; python_version <= '3.7'
+    'joblib<1.3.0; python_version <= 3.7'
 ]
 
 with open(README_FILE, 'r', encoding='utf-8') as f:
