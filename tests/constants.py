3a8b3830e33779a2267a9d4497b6175a2516809d
Ilya Matiach <ilmat@microsoft.com>
2023-11-02 14:19:55 -0400

    fix wrapper error for object detection models on empty model predictions (#158)

--- tests/constants.py
+++ tests/constants.py
@@ -2,6 +2,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
 
+
+UTF8 = 'utf-8'
+
+
 class DatasetConstants(object):
     """Dataset related constants."""
     CATEGORICAL = 'categorical'
